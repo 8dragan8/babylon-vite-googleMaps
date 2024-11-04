5946e8bf9d8fd77168b6d1162d92f98127a2ca26
8dragan8 <35200825+8dragan8@users.noreply.github.com>
2024-11-04 09:56:27 +0100

    update packages

--- src/cesium.ts
+++ src/cesium.ts
@@ -1,4 +1,4 @@
-import * as BABYLON from 'babylonjs'
+import { Color3, Color4, Engine, FreeCamera, Matrix, MeshBuilder, Quaternion, Scene, StandardMaterial, TransformNode, Vector3 } from '@babylonjs/core'
 import * as Cesium from 'cesium'
 
 const canvas = document.querySelector('#webgl') as never as HTMLCanvasElement
@@ -7,7 +7,8 @@ const canvas = document.querySelector('#webgl') as never as HTMLCanvasElement
 const LNG = -121.879057
 const LAT = 37.3416851
 
-const googleAPIurl = 'https://tile.googleapis.com/v1/3dtiles/root.json?key=AIzaSyBxJ2n9B9AAjyFXdoIg1O8Akm0P4HTXx_4'
+const googleAPIKey = 'AIzaSyBxJ2n9B9AAjyFXdoIg1O8Akm0P4HTXx_4'
+// const googleAPIurl = `https://tile.googleapis.com/v1/3dtiles/root.json?key=${googleAPIKey}`
 
 const options: Cesium.Viewer.ConstructorOptions = {
   useDefaultRenderLoop: false,
@@ -21,7 +22,7 @@ const options: Cesium.Viewer.ConstructorOptions = {
   timeline: false,
   fullscreenButton: false,
   mapProjection: new Cesium.WebMercatorProjection(),
-  // allowTextureFilterAnisotropic: false,
+  allowTextureFilterAnisotropic: false,
   contextOptions: {
     webgl: {
       alpha: false,
@@ -33,30 +34,31 @@ const options: Cesium.Viewer.ConstructorOptions = {
     },
   },
   targetFrameRate: 60,
-  // resolutionScale: 0.1,
+  resolutionScale: 0.1,
   orderIndependentTranslucency: true,
-  // imageryProvider: undefined,
+  imageryProvider: undefined,
   baseLayerPicker: false,
   geocoder: false,
   automaticallyTrackDataSourceClocks: false,
-  // clock: null,
+  clock: null,
   terrainShadows: Cesium.ShadowMode.DISABLED,
 }
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyMjhlZWFhZi0xZjVhLTQ1MDktOGI2MS1hYWJmMTYxMGQ0MDciLCJpZCI6MTA0Nzk1LCJpYXQiOjE2NjA1NzQwNTh9.2qUxvkUtmh_LBBDqvJXeaGJ0S27B-DsPNFk3SteOeeg'
 
 export default class CesiumScene {
   viewer!: Cesium.Viewer
-  base_point!: BABYLON.Vector3
-  base_point_up!: BABYLON.Vector3
-  root_node!: BABYLON.TransformNode
-  camera!: BABYLON.FreeCamera
-  scene!: BABYLON.Scene
+  base_point!: Vector3
+  base_point_up!: Vector3
+  root_node!: TransformNode
+  camera!: FreeCamera
+  scene!: Scene
 
-  engine!: BABYLON.Engine
+  engine!: Engine
 
   constructor() {
-    this.initCesium3D().then(() => {
+    this.initCesium().then(() => {
       this.initBabylon()
+      this.addModelToCesium()
       this.engine.runRenderLoop(() => {
         this.viewer.render()
         this.moveBabylonCamera()
@@ -69,7 +71,7 @@ export default class CesiumScene {
     this.viewer = new Cesium.Viewer('cesiumContainer', options)
 
     try {
-      const tileset = await Cesium.Cesium3DTileset.fromUrl(googleAPIurl)
+      const tileset = await Cesium.createGooglePhotorealistic3DTileset(googleAPIKey)
       this.viewer.scene.primitives.add(tileset)
     }
     catch (error) {
@@ -90,6 +92,14 @@ export default class CesiumScene {
     this.base_point_up = this.cart2vec(Cesium.Cartesian3.fromDegrees(LNG, LAT, 300))
   }
 
+  async addModelToCesium() {
+    const resource = await Cesium.IonResource.fromAssetId(2801507)
+    this.viewer.entities.add({
+      model: { uri: resource },
+      position: Cesium.Cartesian3.fromDegrees(-121.879, 37.3418, 0),
+    })
+  }
+
   async initCesium() {
     // const terrainProvider = await Cesium.createWorldTerrainAsync()
 
@@ -108,28 +118,28 @@ export default class CesiumScene {
   }
 
   initBabylon() {
-    const engine = new BABYLON.Engine(canvas, true, {
+    const engine = new Engine(canvas, true, {
       useHighPrecisionFloats: true,
       useHighPrecisionMatrix: true,
     })
-    const scene = new BABYLON.Scene(engine)
-    scene.clearColor = new BABYLON.Color4(0, 0, 0, 0)
+    const scene = new Scene(engine)
+    scene.clearColor = new Color4(0, 0, 0, 0)
 
-    const camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0, 0, -10), scene)
+    const camera = new FreeCamera('camera', new Vector3(0, 0, -10), scene)
 
-    this.root_node = new BABYLON.TransformNode('BaseNode', scene)
+    this.root_node = new TransformNode('BaseNode', scene)
     this.root_node.lookAt(this.base_point_up.subtract(this.base_point))
     this.root_node.addRotation(Math.PI / 2, 0, 0)
 
-    this.root_node.position = new BABYLON.Vector3(0, 0, 0)
-    const box = BABYLON.MeshBuilder.CreateBox('box', { size: 10 }, scene)
-    const material = new BABYLON.StandardMaterial('Material', scene)
-    material.emissiveColor = new BABYLON.Color3(1, 0, 0)
+    this.root_node.position = new Vector3(0, 0, 0)
+    const box = MeshBuilder.CreateBox('box', { size: 10 }, scene)
+    const material = new StandardMaterial('Material', scene)
+    material.emissiveColor = new Color3(1, 0, 0)
     material.alpha = 0.5
     box.material = material
     box.parent = this.root_node
 
-    const ground = BABYLON.MeshBuilder.CreateGround('ground', {
+    const ground = MeshBuilder.CreateGround('ground', {
       width: 100,
       height: 100,
     }, scene)
@@ -150,7 +160,7 @@ export default class CesiumScene {
     this.camera.fov = fov / 180 * Math.PI
 
     const civm = this.viewer.camera.inverseViewMatrix.clone()
-    const camera_matrix = BABYLON.Matrix.FromValues(
+    const camera_matrix = Matrix.FromValues(
       civm[0],
       civm[1],
       civm[2],
@@ -169,9 +179,9 @@ export default class CesiumScene {
       civm[15],
     )
 
-    const scaling = BABYLON.Vector3.Zero()
-    const rotation = BABYLON.Quaternion.Zero()
-    const transform = BABYLON.Vector3.Zero()
+    const scaling = Vector3.Zero()
+    const rotation = Quaternion.Zero()
+    const transform = Vector3.Zero()
 
     camera_matrix.decompose(scaling, rotation, transform)
 
@@ -187,7 +197,7 @@ export default class CesiumScene {
     rotation_y = Math.PI / 2 - rotation_y
 
     const rotation_x = Math.asin(-camera_direction.y)
-    const camera_up_before_rotatez = new BABYLON.Vector3(-Math.cos(rotation_y), 0, Math.sin(rotation_y))
+    const camera_up_before_rotatez = new Vector3(-Math.cos(rotation_y), 0, Math.sin(rotation_y))
     let rotation_z = Math.acos(camera_up.x * camera_up_before_rotatez.x + camera_up.y * camera_up_before_rotatez.y + camera_up.z * camera_up_before_rotatez.z)
 
     rotation_z = Math.PI / 2 - rotation_z
@@ -203,23 +213,46 @@ export default class CesiumScene {
     this.camera.rotation.z = rotation_z
   }
 
-  cart2vec(cart: Cesium.Cartesian3): BABYLON.Vector3 {
-    return new BABYLON.Vector3(cart.x, cart.z, cart.y)
+  cart2vec(cart: Cesium.Cartesian3): Vector3 {
+    return new Vector3(cart.x, cart.z, cart.y)
   }
 
   multiply(mat1: any, mat2: number[], result: any): number[] {
     const m = mat1
     const otherM = mat2
     const offset = 0
-    const tm0 = m[0]; const tm1 = m[1]; const tm2 = m[2]; const tm3 = m[3]
-    const tm4 = m[4]; const tm5 = m[5]; const tm6 = m[6]; const tm7 = m[7]
-    const tm8 = m[8]; const tm9 = m[9]; const tm10 = m[10]; const tm11 = m[11]
-    const tm12 = m[12]; const tm13 = m[13]; const tm14 = m[14]; const tm15 = m[15]
-
-    const om0 = otherM[0]; const om1 = otherM[1]; const om2 = otherM[2]; const om3 = otherM[3]
-    const om4 = otherM[4]; const om5 = otherM[5]; const om6 = otherM[6]; const om7 = otherM[7]
-    const om8 = otherM[8]; const om9 = otherM[9]; const om10 = otherM[10]; const om11 = otherM[11]
-    const om12 = otherM[12]; const om13 = otherM[13]; const om14 = otherM[14]; const om15 = otherM[15]
+    const tm0 = m[0]
+    const tm1 = m[1]
+    const tm2 = m[2]
+    const tm3 = m[3]
+    const tm4 = m[4]
+    const tm5 = m[5]
+    const tm6 = m[6]
+    const tm7 = m[7]
+    const tm8 = m[8]
+    const tm9 = m[9]
+    const tm10 = m[10]
+    const tm11 = m[11]
+    const tm12 = m[12]
+    const tm13 = m[13]
+    const tm14 = m[14]
+    const tm15 = m[15]
+    const om0 = otherM[0]
+    const om1 = otherM[1]
+    const om2 = otherM[2]
+    const om3 = otherM[3]
+    const om4 = otherM[4]
+    const om5 = otherM[5]
+    const om6 = otherM[6]
+    const om7 = otherM[7]
+    const om8 = otherM[8]
+    const om9 = otherM[9]
+    const om10 = otherM[10]
+    const om11 = otherM[11]
+    const om12 = otherM[12]
+    const om13 = otherM[13]
+    const om14 = otherM[14]
+    const om15 = otherM[15]
 
     result[offset] = tm0 * om0 + tm1 * om4 + tm2 * om8 + tm3 * om12
     result[offset + 1] = tm0 * om1 + tm1 * om5 + tm2 * om9 + tm3 * om13
